220f4ba88fea20d542afffdd5c62fb141c5d4efb
NdiayeOusmaneCamara <167888415+NdiayeOusmanaCamara@users.noreply.github.com>
2024-09-19 16:11:08 +0000

    update customer and router docs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 
 import { createApp } from 'vue';
