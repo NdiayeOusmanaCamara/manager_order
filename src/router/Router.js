a65d52e005a84021ebe53ee3221f66deb890dd75
NdiayeOusmaneCamara <167888415+NdiayeOusmanaCamara@users.noreply.github.com>
2024-09-19 11:09:35 +0000

    Added ListCustomer file

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CustomerAdd from "@/components/Customer/AddCustomer.vue";
+import CustomerAdd from "@/components/customer/customer/AddCustomer.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
