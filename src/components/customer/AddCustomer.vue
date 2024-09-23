3d9755aa7afdc29998334b4597be05ffac654fbe
NdiayeOusmaneCamara <167888415+NdiayeOusmanaCamara@users.noreply.github.com>
2024-09-23 01:18:15 +0000

    update addcustomer

--- src/components/customer/AddCustomer.vue
+++ src/components/customer/AddCustomer.vue
@@ -3,30 +3,31 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">  
-          <h5 class="modal-title">Add New Customer</h5>
+          <h5 class="modal-title">Create new customer</h5>
           <button type="button" class="btn-close" @click="$emit('close')"></button>
         </div>
         <div class="modal-body">
           <form @submit.prevent="submit">
-            <div class="form-group">
+            <div class="form-group  pt-2">
               <label for="name">Customer Name</label>
               <input v-model="newCustomer.name" type="text" class="form-control" required />
             </div>
-            <div class="form-group">
-              <label for="address">Address</label>
-              <input v-model="newCustomer.address" type="text" class="form-control" required />
+            <div class="form-group  pt-2">
+              <label for="address ">Address</label>
+              <textarea  v-model="newCustomer.address" type="text" class="form-control" required ></textarea>
             </div>
-            <div class="form-group">
+            <div class="form-group  pt-2">
               <label for="email">Email</label>
               <input v-model="newCustomer.email" type="email" class="form-control" required />
             </div>
-            <div class="form-group">
+            <div class="form-group pt-2">
               <label for="phone">Phone</label>
               <input v-model="newCustomer.phone" type="tel" class="form-control" required />
             </div>
-            <div class="modal-footer">
-              <button type="submit" class="btn btn-primary">Save</button>
-              <button type="button" class="btn btn-secondary" @click="$emit('close')">Cancel</button>
+            <div class="modal-footer mt-5">
+              <button type="button" class="btn btn-secondary" @click="$emit('close')">Close</button>
+              <button type="submit" class="btn btn-primary">Confirm</button>
+              
               
             </div>
           </form>
@@ -53,8 +54,11 @@ const submit = () => {
   background-color: rgba(0, 0, 0, 0.5);
   
 }
+.form-group label{
+padding-bottom: 5px
+}
 .modal-content{
-  margin-top: 200px;
+  margin-top: 150px;
   width: 500px;
   display: flex;
   justify-content: center;
